bb24001909319475a724a35b145c4098fb498c6d
"Liza Nickonova" <liza.nickonova@mail.ru>
2025-06-11 10:58:53 +0300

    back to onrender

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 // https://nodejs-web-server2.onrender.com
 // 'http://localhost:3500'
 const api = axios.create({
-  baseURL: import.meta.env.REACT_APP_API_BASE_URL || 'http://localhost:3500',
+  baseURL: import.meta.env.REACT_APP_API_BASE_URL || 'https://nodejs-web-server2.onrender.com',
   // You can add other default configs here, like headers
 });
 
